070a1940dd5550a537cb30f7b0229c05b878b862
hyperchessbot <hyperchessbot@gmail.com>
2020-12-28 09:57:11 +0000

    Use tera templates

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,13 @@
 
 #[macro_use] extern crate rocket;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
-}
-
 use rocket_contrib::templates::Template;
 use std::collections::HashMap;
 
-#[get("/template")]
-fn template() -> Template {
+#[get("/")]
+fn index() -> Template {
     let mut context = HashMap::<String, String>::new();
-	context.insert("foo".to_string(), "bar".to_string());
+	context.insert("welcome".to_string(), "Welcome to Rocket !".to_string());
     Template::render("index", &context)
 }
 
@@ -21,8 +16,8 @@ use rocket_contrib::serve::StaticFiles;
 
 fn main() {
     rocket::ignite()
-		.mount("/", routes![index, template])
-		.mount("/", StaticFiles::from("."))
+		.mount("/", routes![index])
+		.mount("/", StaticFiles::from("./public"))
 		.attach(Template::fairing())
 		.launch();
 }
