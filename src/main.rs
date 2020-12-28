4a1c1b28a6fa0117abf6a99df8e089dc4b582a48
hyperchessbot <hyperchessbot@gmail.com>
2020-12-28 01:51:10 +0000

    Template

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,22 @@ fn index() -> &'static str {
     "Hello, world!"
 }
 
+use rocket_contrib::templates::Template;
+use std::collections::HashMap;
+
+#[get("/template")]
+fn template() -> Template {
+    let mut context = HashMap::<String, String>::new();
+	context.insert("foo".to_string(), "bar".to_string());
+    Template::render("index", &context)
+}
+
 use rocket_contrib::serve::StaticFiles;
 
 fn main() {
     rocket::ignite()
-		.mount("/", routes![index])
+		.mount("/", routes![index, template])
 		.mount("/", StaticFiles::from("."))
+		.attach(Template::fairing())
 		.launch();
 }
